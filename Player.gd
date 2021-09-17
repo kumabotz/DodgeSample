1a1c792fa5d51028c92f3124e64028c87ac638ef
Tery Lim <kumabotz@gmail.com>
2021-09-17 21:50:44 +1200

    Move by touch

--- Player.gd
+++ Player.gd
@@ -5,23 +5,29 @@ signal hit
 # Declare member variables here.
 export var speed = 400 # pixels/sec
 var screen_size
+var target = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	screen_size = get_viewport_rect().size
 	hide()
+	screen_size = get_viewport_rect().size
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var velocity = Vector2()  # The player's movement vector.
-	if Input.is_action_pressed("ui_right") || Input.is_key_pressed(KEY_D): 
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left") || Input.is_key_pressed(KEY_A):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down") || Input.is_key_pressed(KEY_S):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up") || Input.is_key_pressed(KEY_W):
-		velocity.y -= 1
+	
+	if position.distance_to(target) > 10:
+		velocity = target - position
+
+#	if Input.is_action_pressed("ui_right") || Input.is_key_pressed(KEY_D): 
+#		velocity.x += 1
+#	if Input.is_action_pressed("ui_left") || Input.is_key_pressed(KEY_A):
+#		velocity.x -= 1
+#	if Input.is_action_pressed("ui_down") || Input.is_key_pressed(KEY_S):
+#		velocity.y += 1
+#	if Input.is_action_pressed("ui_up") || Input.is_key_pressed(KEY_W):
+#		velocity.y -= 1
+
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
@@ -46,6 +52,9 @@ func _process(delta):
 		# See the note below about boolean assignment
 		$AnimatedSprite.flip_h = velocity.x < 0
 
+func _input(event):
+	if event is InputEventScreenTouch and event.pressed:
+		target = event.position
 
 func _on_Player_body_entered(body):
 	hide()
@@ -54,5 +63,6 @@ func _on_Player_body_entered(body):
 	
 func start(pos):
 	position = pos
+	target = pos
 	show()
 	$CollisionShape2D.disabled = false
